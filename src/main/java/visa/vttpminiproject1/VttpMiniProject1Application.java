9300f633795653c609d9bbb6826476269cda9e4d
Zheng_Zhi_Qiang <zqzheng98@gmail.com>
2023-12-13 17:15:27 +0800

    initial commit

---
+++ src/main/java/visa/vttpminiproject1/VttpMiniProject1Application.java
@@ -0,0 +1,13 @@
+package visa.vttpminiproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpMiniProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpMiniProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/visa/vttpminiproject1/VttpMiniProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package visa.vttpminiproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpMiniProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
